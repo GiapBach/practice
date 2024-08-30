29e5f679ee547914eb12bd9009ed6041c43413b9
imacbig08 <ngocbach.giap@g.northernacademy.org>
2024-08-30 14:03:38 -0400

    skibidi

--- routes/entriesRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const entriesController = require('../controllers/entriesController');
-
-router.get('/entries', entriesController.fetchEntries);
-
-module.exports = router;
